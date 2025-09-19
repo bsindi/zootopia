8f5932b8ad749dc79a076c996525c3a22ecece9d
bsindi <barzan.sindi@hotmail.com>
2025-09-19 13:05:17 +0200

    git commit -m "verbessere HTML der Karten-Elemente"

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -15,12 +15,14 @@ with open("animals_template.html", "r", encoding="utf-8") as f:
 # Generate string with animal information
 output = ""
 for animal in animals_data:
-    output += '<li class="cards__item">'
-    output += f"Name: {animal['name']}<br/>\n"
-    output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
-    output += f"Location: {animal['locations'][0]}<br/>\n"
-    output += f"Type: {animal['characteristics'].get('type', 'Unknown')}<br/>\n"
-    output += "</li>"
+    output += "<li class='cards__item'>"
+    output += f"<div class='card__title'>{animal['name']}</div>\n"
+    output += f"<p class='card__text'>\n"
+    output += f"<strong>Diet:</strong> {animal['characteristics']['diet']}<br/>\n"
+    output += f"<strong>Location:</strong> {animal['locations'][0]}<br/>\n"
+    output += f"<strong>Type: </strong>{animal['characteristics'].get('type', 'Unknown')}<br/>\n"
+    output += "</p>"
+    output += "</li>\n"
 
 
 #Replace placeholder with animal data
